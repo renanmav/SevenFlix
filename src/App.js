653f469a63790fcc2d515b972e58f7ca93fc65ce
Renan Machado <renan.mav@hotmail.com>
2018-12-11 11:23:22 -0300

    Reacttotron config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import "./config/reactotronConfig";
 
 import Home from "./pages/Home";
+
+console.tron.log("Hello World");
 
 export default class App extends Component {
   render() {
